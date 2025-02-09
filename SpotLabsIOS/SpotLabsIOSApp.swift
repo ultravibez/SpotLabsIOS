fd4bc956877186ebe117296694b11eb8105368ca
Matan Dahan <ultravibez@hotmail.com>
2025-02-09 13:15:50 +0200

    Initial Commit

---
+++ SpotLabsIOS/SpotLabsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotLabsIOSApp.swift
+//  SpotLabsIOS
+//
+//  Created by Matan Dahan on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotLabsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
